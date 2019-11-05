dcf6cd915b86e30356fb09f5af132b31eaa16d08
yehudaclinton <yehudaclinton@gmail.com>
2019-11-05 13:08:42 -0500

    cleaned and fixed

--- battleship.js
+++ battleship.js
@@ -7,7 +7,7 @@ Scope
 -select
 -checkfire
 
-TODO fix toggle select, sound, ai, visibility, multiplayer
+TODO add toggle-select curser, sound, visible, ai, multiplayer
 */
 
 //Board Dimension 
@@ -30,7 +30,7 @@ for (var i=0; i<y; i++){
     }
 }
 
-//should use class like https://www.digitalocean.com/community/tutorials/understanding-classes-in-javascript
+//the ships could have used json
 var units = {
   british_carrier: {
     visibility: 3,
@@ -52,7 +52,7 @@ var units = {
     moverange: 2,
     hitpoints: 3,
     sinking: "battleship_hit.png"
-  }
+  } // TODO correct sinking img and more ships
 };
 
 //drag and drop
@@ -60,7 +60,7 @@ function allowDrop(ev) {
   ev.preventDefault();
 }
 function drag(ev) {
-  ev.dataTransfer.setData("shipid", ev.target.id);//text
+  ev.dataTransfer.setData("shipid", ev.target.id);
   ev.dataTransfer.setData("startloc", ev.target.parentNode.id);
 }
 function drop(ev, loc) {
@@ -81,47 +81,21 @@ function checkmove(unit, loc, start){
 //space already occupied
   var tile = document.getElementById(loc).getElementsByTagName('img')[0];
 
-
   if(start=="") return true;//placing units at start of game
 
   if(tile==undefined && distance <= units[unit].moverange && units[unit].moved != true){
     units[unit].moved = true;
     return true;
   }else{
-    console.log("illegal!! already moved or not your turn");
+    console.log("illegal move!! or not your turn");
   }
 }
-
 var selected = null;
-function select(e){//tile or ship -------------------------------
-  if(e.target.childNodes[0]){//selected tile-- I need to allow select empty and miss
-console.log("select tile");
-    /*if(selected==null){//selected tile to use ship --- disable this function
-      selected = { ship: e.target.childNodes[0].id, location: e.target.id };
-      console.log("Selected: "+JSON.stringify(selected));
-      //e.target.style.backgroundImage = "url('0_6.png')";
-      document.getElementById(e.target.id).classList.add('selected');//////////////////////
-      document.body.classList.add('cursor');
-    }else*/ if(selected){ //if deselecting ship //select!=null and its the same as current select
-      if(selected.location==e.target.id){
-        document.body.style.cursor = null;//"default";
-      //tile background to default style
 
-console.log("deselecting1");
-//document.getElementById(e.target.id).add('selected');
-
-        selected = null;
-
-      
-      }else{//fire
-        checkfire(selected, e.target.id);
-        selected = null
-      }
-    }
-  }else{//clicked on ship ------------/-/-/-/-/-//-/-/-/--------------
+function select(e){//clicked on ship--
 console.log("click on ship itself");
 
-    if(selected==null){//selected tile to use ship
+    if(selected==null){
       selected = { ship: e.target.id, location: e.target.parentNode.id };
       console.log("Selected: "+JSON.stringify(selected));
       //e.target.style.backgroundImage = "url('0_6.png')";
@@ -131,18 +105,14 @@ console.log("click on ship itself");
     }else if(selected){ //if deselecting ship //select!=null and its the same as current select
       if(selected.location==e.target.parentNode.id){
         document.body.style.cursor = null;//null//"default";
-
-console.log("deselecting2");
-        selected = null;
-
-      
       }else{//fire
         checkfire(e.target, e.target.parentNode);
-        selected = null
       }
+
+console.log("deselecting");
+        selected = null;
     }
 
-  }
 }//end of select function
 function checkfire(e, target){//need target ship besides location
   //if hit
@@ -164,13 +134,14 @@ function checkfire(e, target){//need target ship besides location
     hit.play();
     console.log("hit! from "+selected.location);
     console.log(e.id+": hitpoints left:"+units[e.id].hitpoints);
-    if(units[e].hitpoints <1){
+    if(units[e.id].hitpoints <1){
       document.getElementById(e.id).src = units[e.id].sinking;
       document.getElementById(e.id).classList.add("sunk");
     }
   }
 }
 function next(){//next turn reset //maybe i should stop using objects and convert to array
+  selected = null;
   for(x=0; x<Object.keys(units).length; x++){
     Object.values(units)[x].moved = false;
     Object.values(units)[x].fired = false;
